3c1c66adbb309afa97d15b53f0c7a26412a09bba
mochuelo-kun <mochuelokun@gmail.com>
2024-03-03 18:04:18 -0800

    fix server start command (poetry + uvicorn)

--- src/api/main.py
+++ src/api/main.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -12,3 +13,8 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+
+def start_server():
+    """Launched with `poetry run start_server` at root level"""
+    uvicorn.run("src.api.main:app", host="0.0.0.0", port=8000, reload=True)
